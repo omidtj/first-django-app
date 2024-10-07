62eb8e138979b9d2b535f67e1de5d0f041f467b6
OMID <omid.tajer@gmail.com>
2024-10-07 17:29:20 +0330

    create template
    html template for pages

--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from website.views import *
 urlpatterns = [
     # ("url address","view")
-    # just for test
-    path('json-test', json_test),
-    # like real project
     path('', home_view),
     path('about', about_view),
     path('contact', contact_view)
--- website/views.py
+++ website/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
-# for test
-def json_test(request):
-    return JsonResponse({'name':'ali','sur-name':'bigdeli'})
-# for real
+# آدرس شروع از فولدر تمپلیتس
 def home_view(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request,'website/index.html')
 def about_view(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request,'website/about.html')
 def contact_view(request):
-    return HttpResponse('<h1>Contact Page</h1>')
+    return render(request,'website/contact.html')
+
 
   
