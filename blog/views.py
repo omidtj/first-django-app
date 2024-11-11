a09504db7e25a631e91c3c66051f42ece7d734d2
OMID <omid.tajer@gmail.com>
2024-11-11 19:30:15 +0330

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.utils import timezone
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 # آدرس شروع از فولدر تمپلیتس
@@ -8,6 +8,14 @@ def blog_view(request,**kwargs):
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username') != None :
         posts = posts.filter(author__username=kwargs['author_username'])
+    paginator = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.get_page(1)
+    except EmptyPage:
+        posts = paginator.get_page(1)    
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
