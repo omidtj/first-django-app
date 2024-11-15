85b20c90a0b4975a6ee339cfd42cc7504b0c6948
OMID <omid.tajer@gmail.com>
2024-11-15 12:25:22 +0330

    debug toolbar

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
     'robots',
     'website.apps.WebsiteConfig',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'debug_toolbar'
 ]
 # site framework
 SITE_ID = 1
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -141,3 +143,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
