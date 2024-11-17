6fd692325426d1b21fc15019b10d55f451792953
OMID <omid.tajer@gmail.com>
2024-11-17 11:24:53 +0330

    Comment model added

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post,Category
+from blog.models import Post,Category,Comment
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -19,3 +19,17 @@ class PostAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin): 
     pass  
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin): 
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display =('name',
+                   'post',
+                   'approved',
+                   'created_date',
+                   'updated_date')
+    list_filter = ('post','approved')
+    search_fields=['name','post']
+
+
