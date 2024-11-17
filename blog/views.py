2339aa3a3d6c741c29306095e4cb820cfdfcd77a
OMID <omid.tajer@gmail.com>
2024-11-17 16:44:24 +0330

    Comment Form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post,Comment
+from blog.forms import CommentForm
+from django.contrib import messages
+
 # آدرس شروع از فولدر تمپلیتس
 def blog_view(request,**kwargs):
     posts = Post.get_all_published_posts()
@@ -23,17 +26,30 @@ def blog_view(request,**kwargs):
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            """   obj = form.save(commit=False)
+            obj.name = 'Unknown'
+            obj.save() """
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'your comment submited successfully')
+        else:
+            messages.add_message(request,messages.ERROR,'your comment didnt submited!!')
+    
     published_posts = Post.get_all_published_posts()
     post = get_object_or_404(published_posts,pk=pid)
     comments=Comment.objects.filter(post=post.id,approved=True)
     next_post = post.next_post(published_posts)
     previous_post = post.previous_post(published_posts)
     post.counted_views_Inc()
+    form = CommentForm()
     context = {
-                'post':post ,
-                'next_post':next_post ,
-                'previous_post':previous_post,
-                'comments':comments}
+            'post':post ,
+            'next_post':next_post ,
+            'previous_post':previous_post,
+            'comments':comments,
+            'form':form}
     return render(request,'blog/blog-single.html',context)
 
 def blog_search(request):
