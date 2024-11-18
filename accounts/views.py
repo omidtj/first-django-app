d95072507e9937a7f221c8f122e5c35d6a8f4933
OMID <omid.tajer@gmail.com>
2024-11-18 19:26:40 +0330

    login simple

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def login_view(request):
-    if request.user.is_authenticated:
-        msg=f'{request.user.get_full_name()} is authenticated'
-    else :
-        msg = 'user is not authenticated' 
-    context = {'msg':msg}       
-    return render(request,'accounts/login.html',context)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+           
+    return render(request,'accounts/login.html')
 
 # def logout_view(request):
 #     return render(request,'accounts/logout.html')
