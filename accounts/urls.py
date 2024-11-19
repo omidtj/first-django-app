d5ccac5a8940fac57f699bf92a5f0bf3f893ca51
OMID <omid.tajer@gmail.com>
2024-11-19 12:00:50 +0330

    logout and signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from accounts.views import *
 app_name = 'accounts'
 urlpatterns = [
     # ("url address","view","name for html")
-    path('login', login_view,name='login'),
-    # path('logout', logout_view,name='logout'),
+    path('login/', login_view,name='login'),
+    path('logout', logout_view,name='logout'),
     path('signup', signup_view,name='signup'),
   ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 # Create your views here.
 def login_view(request):
     if not request.user.is_authenticated:
@@ -18,9 +20,23 @@ def login_view(request):
         return render(request,'accounts/login.html',context)
     else:
         return redirect('/')
-# def logout_view(request):
-#     return render(request,'accounts/logout.html')
+
+#حتما باید لاگین باشه
+@login_required    
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
 def signup_view(request):
-    return render(request,'accounts/signup.html')
-
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+                form = UserCreationForm(request.POST)
+                if form.is_valid():
+                    form.save()
+                    return redirect('/')
+        form =UserCreationForm()
+        context={'form':form}
+        return render(request,'accounts/signup.html',context)
+    else:
+        return redirect('/')
+        
