3b04588bb1793bcecaafa42c90651248ea920f70
OMID <omid.tajer@gmail.com>
2024-11-20 21:52:58 +0330

    tamrin 1.1

--- accounts/views.py
+++ accounts/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
+from django.contrib.auth.models import User
+from django.db.models import Q
 # Create your views here.
 def login_view(request):
     if not request.user.is_authenticated:
+        form=AuthenticationForm() #get
         if request.method == 'POST':
-            form = AuthenticationForm(request=request,data=request.POST)
-            if form.is_valid():
-                username= form.cleaned_data.get('username')
-                password= form.cleaned_data.get('password')
-                user = authenticate(username=username, password=password)
+            form = AuthenticationForm(request=request,data=request.POST) #post
+            username= request.POST['username']
+            password= request.POST['password']
+            user_data = User.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+            user = authenticate(username=user_data.username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('/')
-        form=AuthenticationForm()
         context={'form':form}    
         return render(request,'accounts/login.html',context)
     else:
@@ -32,7 +33,15 @@ def signup_view(request):
         if request.method == 'POST':
                 form = UserCreationForm(request.POST)
                 if form.is_valid():
-                    form.save()
+                    username= request.POST['username']
+                    password= request.POST['password1']
+                    email= request.POST['email']
+                    first_name= request.POST['first_name']
+                    last_name= request.POST['last_name']
+                    user = User.objects.create_user(username,email,password)
+                    user.first_name = first_name
+                    user.last_name = last_name
+                    user.save()
                     return redirect('/')
         form =UserCreationForm()
         context={'form':form}
