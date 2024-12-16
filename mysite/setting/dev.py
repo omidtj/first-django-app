687bb3d56c6e6a844e3ba432d23a8cfe69265bae
OMID <omid.tajer@gmail.com>
2024-12-16 12:15:23 +0330

    Production step 1
    separation between dev and pod
    dev > set manage.py setting
    pod > set wsgi.py setting

---
+++ mysite/setting/dev.py
@@ -0,0 +1,40 @@
+from mysite.settings import *
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-05_j_ipj_hjn9_)c0+_&uz@*@u1h0g#p+7j+kq=)#*hqv_w)ju'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# INSTALLED_APPS =[]
+
+# site framework
+SITE_ID = 1
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+
+# EMAIL Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'omidweb67@gmail.com'
+EMAIL_HOST_PASSWORD = 'zktu hcfl pzjk gjox'
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,18 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-05_j_ipj_hjn9_)c0+_&uz@*@u1h0g#p+7j+kq=)#*hqv_w)ju'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -49,8 +37,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'accounts'
 ]
-# site framework
-SITE_ID = 1
+
 # robots
 ROBOTS_USE_HOST = False
 ROBOTS_USE_SITEMAP = True
@@ -90,16 +77,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -138,14 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -155,10 +127,3 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-# EMAIL Settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'omidweb67@gmail.com'
-EMAIL_HOST_PASSWORD = 'zktu hcfl pzjk gjox'
